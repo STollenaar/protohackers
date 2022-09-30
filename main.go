8b78dc7c443333ca0ba2a9eb5cb1adcd01f18acb
Stephen Tollenaar <stephen.tollenaar@radient360.com>
2022-09-29 22:06:32 -0230

    adding env variable for the port

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 )
 
 func main() {
-	ln, err := net.Listen("tcp", ":10000")
+	ln, err := net.Listen("tcp", ":"+os.Getenv("PORT"))
 	if err != nil {
 		fmt.Println("listen: ", err.Error())
 		os.Exit(1)
 	}
-	fmt.Println("listening on port 10000")
+	fmt.Println("listening on port " + os.Getenv("PORT"))
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
